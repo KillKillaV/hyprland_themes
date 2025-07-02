d129e070d1ae9c08df7dca0297fa7f40c83fab9e
KillKillaV <volgm310per@gmail.com>
2025-07-02 21:02:12 +0200

    kitty3

--- theme_1/install.sh
+++ theme_1/install.sh
@@ -157,7 +157,7 @@ WAY="$HOME/.config/waypaper"
 WAYPAPER="$HOME/hyprland_themes/theme_1/waypaper"
 FAST="$HOME/.config/fastfetch"
 FASTFETCH="$HOME/hyprland_themes/theme_1/fastfetch"
-KI="$HOME/.config/kitty"
+KI="$HOME/.config/"
 TTY="$HOME/hyprland_themes/theme_1/kitty"
 
 # Detectar Hypr
